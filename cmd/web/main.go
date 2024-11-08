328dc0a8a2f85bc3e554b5d055fc3ca836cc3d74
Stanley Lee <stanley.lws10@gmail.com>
2024-11-08 19:00:38 +0800

    Decode form helper

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,14 +10,16 @@ import (
 
 	"snippetbox.stanley.net/internal/models"
 
+	"github.com/go-playground/form/v4"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
-	errorLog *log.Logger
-	infoLog *log.Logger
-	snippets *models.SnippetModel
-	templateCache map[string]*template.Template
+	errorLog 		*log.Logger
+	infoLog 		*log.Logger
+	snippets 		*models.SnippetModel
+	templateCache	map[string]*template.Template
+	formDecoder 	*form.Decoder
 }
 
 func main() {
@@ -40,11 +42,14 @@ func main() {
 		errorLog.Fatal(err)
 	}
 
+	formDecoder := form.NewDecoder()
+
 	app := &application{
 		errorLog: errorLog,
 		infoLog: infoLog,
 		snippets: &models.SnippetModel{DB: db},
 		templateCache: templateCache,
+		formDecoder: formDecoder,
 	}
 
 	srv := &http.Server{

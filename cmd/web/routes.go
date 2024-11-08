6fbb2cf50c9623ec246055a33e485b608cc3a22b
Stanley Lee <stanley.lws10@gmail.com>
2024-11-08 16:34:13 +0800

    Fix static route

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -14,11 +14,11 @@ func (app *application) routes() http.Handler {
 	})
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	router.Handler(http.MethodGet, "/static/", http.StripPrefix("/static", fileServer))
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
 	router.HandlerFunc(http.MethodGet, "/", app.home)
 	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
-	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreateForm)
 	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
 
 	return app.recoverPanic(app.logRequest(secureHeaders(router)))

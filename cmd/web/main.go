b7b0c8ce4b81abaa52f0a6aa061fb0b6f8afa860
Stanley Lee <stanley.lws10@gmail.com>
2024-11-24 00:00:07 +0800

    Add user models

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -22,9 +22,10 @@ type application struct {
 	errorLog 		*log.Logger
 	infoLog 		*log.Logger
 	snippets 		*models.SnippetModel
+	users			*models.UserModel
 	templateCache	map[string]*template.Template
 	formDecoder 	*form.Decoder
-	sessionManager *scs.SessionManager
+	sessionManager	*scs.SessionManager
 }
 
 func main() {
@@ -58,6 +59,7 @@ func main() {
 		errorLog: errorLog,
 		infoLog: infoLog,
 		snippets: &models.SnippetModel{DB: db},
+		users: &models.UserModel{DB: db},
 		templateCache: templateCache,
 		formDecoder: formDecoder,
 		sessionManager: sessionManager,

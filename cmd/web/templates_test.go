297e1ad1ee830e00f90642d3b14039d90b737570
Stanley Lee <stanley.lws10@gmail.com>
2024-11-27 16:11:17 +0800

    Add test for humanDate

---
+++ cmd/web/templates_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestHumanDate(t *testing.T) {
+	tests := []struct {
+		name string
+		tm time.Time
+		want string
+	}{
+		{
+			name: "UTC",
+			tm: time.Date(2024, 11, 27, 10, 15, 0, 0, time.UTC),
+			want: "27 Nov 2024 at 10:15",
+		},
+		{
+			name: "Empty",
+			tm: time.Time{},
+			want: "",
+		},
+		{
+			name: "CET",
+			tm: time.Date(2024, 11, 27, 10, 15, 0, 0, time.FixedZone("UTC+8", 8*60*60)),
+			want: "27 Nov 2024 at 02:15",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			hd := humanDate(tt.tm)
+
+			if hd != tt.want {
+				t.Errorf("got %q; want %q", hd, tt.want)
+			}
+		})
+	}
+}

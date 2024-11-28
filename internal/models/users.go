2c9828f3c9c97dea01cb424af2030551a4b60bcf
Stanley Lee <stanley.lws10@gmail.com>
2024-11-28 14:55:51 +0800

    DB integration test

--- internal/models/users.go
+++ internal/models/users.go
@@ -81,7 +81,7 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 func (m *UserModel) Exists(id int) (bool, error) {
 	var exists bool	
 
-	stmt := "SELECT EXISTS(SELECT true FROM users WHERE is = ?)"
+	stmt := "SELECT EXISTS(SELECT true FROM users WHERE id = ?)"
 
 	err := m.DB.QueryRow(stmt, id).Scan(&exists)
 	return exists, err
